130933638e39b9d1d6e5768c55ecf122452a4d11
Diego Oliveira <dsoliveira@stone.com.br>
2023-10-02 10:29:03 -0300

    incluindo template go gin

--- karavela-go/template-go-gin/src/go.mod
+++ karavela-go/template-go-gin/src/go.mod
@@ -2,4 +2,7 @@ module ${{ values.apiName | lower }}
 
 go ${{ values.goVersion | lower }}
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/sirupsen/logrus v1.9.3
+)
--- karavela-go/template-go-gin/src/main.go
+++ karavela-go/template-go-gin/src/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/sirupsen/logrus"
 )
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+var log = logrus.New()
+
+func init() {
+	log.SetFormatter(&logrus.JSONFormatter{})
+}
 
 func main() {
 	r := gin.Default()
@@ -17,5 +31,25 @@ func main() {
 		c.String(http.StatusOK, "~> ${{ values.apiName | lower }} is healthy.")
 	})
 
-	r.Run(":${{ values.port | lower }}")
+	//r.Run(":${{ values.port | lower }}")
+	r.Run(":3000")
+}
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+
+		log.WithFields(logrus.Fields{
+			"method":      r.Method,
+			"url":         r.URL.Path,
+			"remote_addr": r.RemoteAddr,
+			"duration":    duration,
+			"statusCode":  rw.statusCode,
+		}).Info("Request completed")
+	})
 }
